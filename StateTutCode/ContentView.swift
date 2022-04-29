73779c520eb6dd4fcb25c9bea555ff9ae5729809
Kazimierz Osinski <kazimierz.osinski@gmail.com>
2022-04-29 09:31:56 +0200

    Initial Commit

---
+++ StateTutCode/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  StateTutCode
+//
+//  Created by HAL-9001 on 29/04/2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ StateTutCode/StateTutCodeApp.swift
@@ -0,0 +1,17 @@
+//
+//  StateTutCodeApp.swift
+//  StateTutCode
+//
+//  Created by HAL-9001 on 29/04/2022.
+//
+
+import SwiftUI
+
+@main
+struct StateTutCodeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
